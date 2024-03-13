f55bcf8999e1b4c54e998e75ed682829af55faa0
Zhao Liu <liu.9553@osu.edu>
2024-03-13 19:52:01 -0400

    Initialize Lab 4

---
+++ Lab 4/matrix.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+// You may add other header files if needed
+using namespace std;
+
+template <typename T>
+class  Matrix
+{
+    // Please finish the Matrix template
+    public:
+
+};
